6117f26d88d338aa4993b44b431193fb9c709122
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2022-11-10 15:25:00 +0100

    Working sourcing of the page with every assets

--- ressources/source_sundae.js
+++ ressources/source_sundae.js
@@ -2,26 +2,16 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({ width: 1400, height: 1080}); // We want a big page to be able to see every assets
-  await page.goto('https://exchange.sundaeswap.finance'); // Wait until page is fully loaded
-  await page.evaluate(() => {
-    
-  });
-  await page.evaluate(scrollBottom);
-  await page.evaluate(scrollBottom);
-  await page.evaluate(scrollBottom);
-  await page.evaluate(scrollBottom);
-  await page.evaluate(scrollBottom);
-  await page.evaluate(scrollBottom);
-
-
-  await page.waitForTimeout(5000);
-
-
+  await page.setViewport({ width: 1400, height: 1080}); // We want a big page to be able to see the most assets as possible
+  await page.goto('https://exchange.sundaeswap.finance', {waitUntil: 'networkidle0'}); // Wait until page is fully loaded
+  for (let i = 0; i < 4; i++) {
+    await page.evaluate(scrollToBottom); // Scroll down to load more assets
+    await page.waitForNetworkIdle(1000); // Wait 1s
+  }
   console.log(await page.content()); // out the content
   await browser.close();
 })();
 
-function scrollBottom() {
+function scrollToBottom() {
   window.scrollTo(0, window.document.body.scrollHeight);
 }
