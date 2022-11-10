dc3f18032168ef6599dec13f8b939520adfe3a62
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2022-11-10 18:24:36 +0100

    Greatly improved regular expressions, added a new setting to get a specific asset informations, and get 510 next characters

--- scrape.sh
+++ scrape.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #Installation of needed programs
-if grep -qv 'ask_for_install\s*=\s*false' settings.txt; then
+if [[ $(cat settings.txt | sed -n -e "s/ask_for_install *= *//p") == 'true' ]]; then
     echo "Run program installer (y)/n: "
     read install_requested
-    if [[ install_requested != 'n' ]]; then
+    if [ $install_requested != 'n' ]; then
         bash ./ressources/install.sh
     fi
 fi
 
+#Get the asset to follow inside the settings file
+asset_to_follow=$(cat settings.txt | sed -n -e 's/asset_to_follow *= *//p')
 
 #Scraping and parsing
-node ./ressources/source_sundae.js #| grep "CLAP"
+node ./ressources/source_sundae.js | egrep -o "$asset_to_follow/ADA.{255}.{255}"
