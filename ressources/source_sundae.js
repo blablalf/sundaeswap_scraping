bec032da040d13d707668597298a06e319e7642d
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2022-11-10 15:15:21 +0100

    Scraping more coins with scrolling

--- ressources/source_sundae.js
+++ ressources/source_sundae.js
@@ -2,7 +2,26 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://exchange.sundaeswap.finance', {waitUntil: 'networkidle0'}); // Wait until page is fully loaded
+  await page.setViewport({ width: 1400, height: 1080}); // We want a big page to be able to see every assets
+  await page.goto('https://exchange.sundaeswap.finance'); // Wait until page is fully loaded
+  await page.evaluate(() => {
+    
+  });
+  await page.evaluate(scrollBottom);
+  await page.evaluate(scrollBottom);
+  await page.evaluate(scrollBottom);
+  await page.evaluate(scrollBottom);
+  await page.evaluate(scrollBottom);
+  await page.evaluate(scrollBottom);
+
+
+  await page.waitForTimeout(5000);
+
+
   console.log(await page.content()); // out the content
   await browser.close();
 })();
+
+function scrollBottom() {
+  window.scrollTo(0, window.document.body.scrollHeight);
+}
--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,9 @@
+#Installation of needed programs
 echo "Run program installer (y)/n: "
 read install_requested
-if [[ install_requested != 'n' ]]; then
+if [[ install_requested == 'n' ]]; then
     bash ./ressources/install.sh
 fi
-node ./ressources/source_sundae.js | grep clap
+
+#Scraping and parsing
+node ./ressources/source_sundae.js #| grep "CLAP"
