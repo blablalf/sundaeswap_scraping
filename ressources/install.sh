4fb3fa392682e4233a7007f1f9d6aabb2567f1e9
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2022-11-11 15:40:43 +0100

    Working crontab job, added a .env file which is supported by the installation script and will permit to store sensible datas

--- ressources/install.sh
+++ ressources/install.sh
@@ -10,13 +10,23 @@ if [[ $OSTYPE == 'darwin'* ]]; then
     else
         echo 'brew_detected'
     fi
-    echo 'Installing node & npm with brew'
+    echo 'Installing node & npm & screen with brew'
     brew install node
+
 #Linux install
 elif [[ $OSTYPE == 'linux'* ]]; then
     echo 'linux_detected'
     echo 'Installing node & npm with apt'
     sudo apt install nodejs npm -y
+    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 fi
+
+#Node dependencies installation
 echo 'Installing node dependencies'
 cd ./ressources && npm i
+
+#Getting Node path
+echo 'Getting node path'
+node_path=$(which node)
+sed -ie '/node_path *=.*/d' ../.env #deleting eventual old line
+sed -ie "1s|^|node_path=$node_path\n|" ../.env #adding the new one
--- scrape.sh
+++ scrape.sh
@@ -13,20 +13,24 @@ fi
 
 #Cronjob setting
 #"* * * * * $(dirname $0)/scrape.sh"
-#* * * * * /Users/blabla/sundaeswap_scraping/scrape.sh
+#* * * * * screen -dmS scrape bash "$(dirname $0)/scrape.sh"
+#echo "* * * * * screen -dmS scrape bash \"$(dirname $0)/scrape.sh\"" > ressources/cron_scrape
 
 #Get the asset to follow inside the settings file
 asset_to_follow=$(cat settings.txt | sed -ne 's/asset_to_follow *= *//p')
 
 #Scraping and parsing
-code_to_parse=$(node ./ressources/source_sundae.js)
+node_path=$(cat .env | sed -ne 's/node_path *= *//p') #crontab doesn't know node full path
+code_to_parse=$($node_path ./ressources/source_sundae.js)
 asset_code=$(echo $code_to_parse | egrep -o "$asset_to_follow/ADA.{255}.{255}" | grep -o "<div class=\"sc-bdvvtL sc-1aj876m-6 dfkUWV biWbyS\">.*</div>")
 asset_decimal=$(echo $asset_code | grep -o "<span class=\"sc-lbhJGD hfhuXO\">.*</span> ₳" | sed -n 's:.*huXO">\(.*\)</spa.*:\1:p')
 asset_main_val=$(echo $asset_code | grep -o "<span><span>.*</span><span" | sed -n 's:.*<span>\(.*\)</spa.*:\1:p')
 asset_val=$asset_main_val$asset_decimal'₳'
 
-#Displaying asset val
-message_url="https://api.telegram.org/bot<token>/sendMessage?chat_id=<chatid>&text="$asset_to_follow"_price="$asset_val
+#Displaying asset val with telegram bot
+bot_token=$(cat .env | sed -ne 's/bot_token *= *//p')
+chat_id=$(cat .env | sed -ne 's/chat_id *= *//p')
+message_url="https://api.telegram.org/bot"$bot_token"/sendMessage?chat_id="$chat_id"&text="$asset_to_follow"_price="$asset_val
 
 #Send the message
 curl $message_url
